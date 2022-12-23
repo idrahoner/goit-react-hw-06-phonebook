86bf2648926b02b2e900d43c3fc252d9879730b1
Ivan Drahoner <111982515+idrahoner@users.noreply.github.com>
2022-12-23 17:49:14 +0200

    Refactored reducers with createReducer()

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,37 +1,24 @@
-// import { createReducer } from '@reduxjs/toolkit';
+import { createReducer } from '@reduxjs/toolkit';
 
 import { addContact, removeContact, changeFilter } from './actions';
 
 const initialContacts = { contactList: [] };
 const initialFilter = '';
 
-export const contactsReducer = (state = initialContacts, action) => {
-  switch (action.type) {
-    case addContact.type:
-      return {
-        ...state,
-        contactList: [...state.contactList, action.payload],
-      };
+export const contactsReducer = createReducer(initialContacts, {
+  [addContact]: (state, action) => {
+    state.contactList.push(action.payload);
+  },
+  [removeContact]: (state, action) => {
+    return {
+      ...state,
+      contactList: state.contactList.filter(
+        element => element.id !== action.payload
+      ),
+    };
+  },
+});
 
-    case removeContact.type:
-      return {
-        ...state,
-        contactList: state.contactList.filter(
-          element => element.id !== action.payload
-        ),
-      };
-
-    default:
-      return state;
-  }
-};
-
-export const filterReducer = (state = initialFilter, action) => {
-  switch (action.type) {
-    case changeFilter.type:
-      return action.payload;
-
-    default:
-      return state;
-  }
-};
+export const filterReducer = createReducer(initialFilter, {
+  [changeFilter]: (state, action) => action.payload,
+});
